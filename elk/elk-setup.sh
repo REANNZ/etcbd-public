48b2312cad077158fc812293b2a52d85cbfb6102
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2018-08-01 12:00:41 +1200

    ELK: move search queue size changes from setup script to env vars
    Threadpool settings are no longer dynamically updatable.
    
    And a -1 is no longer available as "unlimited".  So just up the queue size from 1000 to 10,000...

--- elk/elk-setup.sh
+++ elk/elk-setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Removing elasticsearch queue limit size..."
-docker exec kibana curl -Ss -XPUT elasticsearch:9200/_cluster/settings -d '{ "persistent" : { "threadpool.search.queue_size" : -1 } }'
-echo "...done"
-
 echo "Loading dashboards and visualizations..."
 docker exec kibana /load_dashboards.sh
 echo "...done"
