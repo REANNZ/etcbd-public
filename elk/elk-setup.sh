776f9f4325d8c8cbe6d07edb2b88a40d5607db18
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2018-08-01 12:03:38 +1200

    elk-setup.sh: configure ES NOT to create replicas

--- elk/elk-setup.sh
+++ elk/elk-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Configuring elasticsearch NOT to create replicas..."
+docker exec elasticsearch curl -Ss -HContent-Type:application/json -XPOST 'localhost:9200/_template/all' -d '{ "template": "*", "settings": { "number_of_replicas": 0 } }'
+
 echo "Loading dashboards and visualizations..."
 docker exec kibana /load_dashboards.sh
 echo "...done"
