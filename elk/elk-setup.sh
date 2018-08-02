92bae9a0f2274922176e9ea2c54f1ef5b299bba4
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2018-08-02 12:02:44 +1200

    elk-setup.sh: silence JVM GC log messages

--- elk/elk-setup.sh
+++ elk/elk-setup.sh
@@ -3,6 +3,9 @@
 echo "Configuring elasticsearch NOT to create replicas..."
 docker exec elasticsearch curl -Ss -HContent-Type:application/json -XPOST 'localhost:9200/_template/all' -d '{ "template": "*", "settings": { "number_of_replicas": 0 } }'
 
+echo "Silencing JVM GC logs"
+docker exec elasticsearch curl -Ss -HContent-Type:application/json -XPUT elasticsearch:9200/_clu'{ "persistent" : { "logger.org.elasticsearch.monitor.jvm.JvmGcMonitorService" : "WARN" } }'
+
 echo "Loading dashboards and visualizations..."
 docker exec kibana /load_dashboards.sh
 echo "...done"
