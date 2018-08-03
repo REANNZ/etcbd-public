c2239e0ff976a46389b8718313b210ab39fe5c08
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2018-08-03 15:58:24 +1200

    elk-setup.sh: fix typo

--- elk/elk-setup.sh
+++ elk/elk-setup.sh
@@ -4,7 +4,7 @@ echo "Configuring elasticsearch NOT to create replicas..."
 docker exec elasticsearch curl -Ss -HContent-Type:application/json -XPOST 'localhost:9200/_template/all' -d '{ "template": "*", "settings": { "number_of_replicas": 0 } }'
 
 echo "Silencing JVM GC logs"
-docker exec elasticsearch curl -Ss -HContent-Type:application/json -XPUT elasticsearch:9200/_clu'{ "persistent" : { "logger.org.elasticsearch.monitor.jvm.JvmGcMonitorService" : "WARN" } }'
+docker exec elasticsearch curl -Ss -HContent-Type:application/json -XPUT elasticsearch:9200/_cluster/settings -d '{ "persistent" : { "logger.org.elasticsearch.monitor.jvm.JvmGcMonitorService" : "WARN" } }'
 
 if [ "$1" == "--force" ] ; then
     echo "Force upgrade - deleting .kibana index"
